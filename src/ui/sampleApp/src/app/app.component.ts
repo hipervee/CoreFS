7ed251e4e60376d0df37d3978dd19e5c1f295a5f
Pervaze Ahanger <pervazea@mumbai1.corp.citiustech.com>
2017-11-10 19:20:32 +0530

    modifications

--- src/ui/sampleApp/src/app/app.component.ts
+++ src/ui/sampleApp/src/app/app.component.ts
@@ -1,30 +1,12 @@
-import { WeatherService } from './weather.service';
 import { Component } from '@angular/core';
 import { SnotifyService } from 'ng-snotify';
-import { CounterService, NotificationService, AsyncConfig } from 'nt-uicore';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [WeatherService]
 })
 export class AppComponent {
   title = 'app';
-  constructor(public counterService: CounterService, private ns: NotificationService, private weatherService: WeatherService) { }
-
-  success() {
-    this.ns.success('This is success message');
-  }
-  incr() {
-    this.counterService.counter += 1;
-  }
-
-  apiCall() {
-    const config = new AsyncConfig('Processing', 'Success', 'Failure');
-    this.ns.async(this.weatherService.getWeather(), config).subscribe(s => {
-    }, f => {
-    });
-    this.ns.async(this.weatherService.getClimate(), config).subscribe(s => {});
-  }
+  constructor() { }
 }
--- src/ui/sampleApp/src/app/notifications/notifications.component.ts
+++ src/ui/sampleApp/src/app/notifications/notifications.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { NotificationService, AsyncConfig } from 'nt-uicore';
+import { WeatherService } from '../weather.service';
+
+declare var $: any;
 
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
-  styleUrls: ['./notifications.component.css']
+  styleUrls: ['./notifications.component.css'],
+  providers: [WeatherService]
 })
 export class NotificationsComponent implements OnInit {
+  constructor(private ns: NotificationService, private ws: WeatherService) { }
 
-  constructor() { }
+  showNotification(from, align) {
+    const type = ['', 'info', 'success', 'warning', 'danger'];
 
+    const color = Math.floor((Math.random() * 4) + 1);
+
+    $.notify({
+      icon: "notifications",
+      message: "Welcome to <b>Material Dashboard</b> - a beautiful freebie for every web developer."
+
+    }, {
+        type: type[color],
+        timer: 4000,
+        placement: {
+          from: from,
+          align: align
+        }
+      });
+  }
   ngOnInit() {
   }
 
+  success() {
+    this.ns.success('Success Message Body', 'Success');
+  }
+
+  info() {
+    this.ns.info('This is info body', 'Information');
+  }
+
+  apiCall() {
+    const config = new AsyncConfig('Processing', 'Success', 'Failure');
+    this.ns.async(this.ws.getWeather(), config).subscribe(s => {
+    }, f => {
+    });
+    this.ns.async(this.ws.getClimate(), config).subscribe(s => { });
+  }
 }
