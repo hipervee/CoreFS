961a65d8766f9e91571124183bf648dce7435bce
Pervaze Ahanger <pervazea@mumbai1.corp.citiustech.com>
2017-11-10 17:07:40 +0530

    add material

--- src/ui/sampleApp/src/app/app.module.ts
+++ src/ui/sampleApp/src/app/app.module.ts
@@ -1,13 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import { AppRoutingModule } from './app.routing';
+import { ComponentsModule } from './components/components.module';
 import { ServicesModule } from 'nt-uicore';
+
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
+    ComponentsModule,
+    RouterModule,
+    AppRoutingModule,
     ServicesModule.forRoot()
   ],
   bootstrap: [AppComponent]
